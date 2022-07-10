d2b7622919927aca324c92672b0e35d431bb9647
Anjan Roy <hello@itzmeanjan.in>
2022-07-10 22:48:04 +0400

    Romulus-N authenticated encryption
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/aead.hpp
+++ include/aead.hpp
@@ -7,6 +7,13 @@
 // Romulus Authenticated Encryption and Hash Function
 namespace romulus {
 
+// Given 16 -bytes secret key, 16 -bytes nonce, N -bytes associated data and M
+// -bytes plain text | N, M >= 0, this routine computes M -bytes encrypted text
+// and 16 -bytes authentication tag, using Romulus-N authenticated encryption
+// algorithm
+//
+// See encryption algorithm defined in figure 2.5 of Romulus specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/romulus-spec-final.pdf
 inline static void encrypt_romulusn(
     const uint8_t* const __restrict key,    // 128 -bit secret key
     const uint8_t* const __restrict nonce,  // 128 -bit public message nonce
@@ -30,67 +37,124 @@ inline static void encrypt_romulusn(
   std::memset(state, 0, 16);
   romulus_common::set_lfsr(lfsr);
 
-  constexpr size_t br0[2] = {0, 1};
+  {
+    constexpr size_t br0[2] = {0, 1};
 
-  const size_t full_blk_cnt = dlen >> 4;
-  const size_t rm_bytes = dlen & 15;
+    const size_t full_blk_cnt = dlen >> 4;
+    const size_t rm_bytes = dlen & 15;
 
-  const bool flg = (dlen == 0) | (rm_bytes > 0);
+    const bool flg = (dlen == 0) | (rm_bytes > 0);
 
-  const size_t tot_blk_cnt = full_blk_cnt + br0[flg];
-  const size_t half_blk_cnt = tot_blk_cnt >> 1;
+    const size_t tot_blk_cnt = full_blk_cnt + br0[flg];
+    const size_t half_blk_cnt = tot_blk_cnt >> 1;
 
-  size_t off = 0;
+    size_t off = 0;
 
-  for (size_t i = 0; i < half_blk_cnt; i++) {
-    uint8_t right_blk[16];
-    std::memset(right_blk, 0, 16);
+    for (size_t i = 0; i < half_blk_cnt; i++) {
+      uint8_t right_blk[16];
+      std::memset(right_blk, 0, 16);
 
-    const size_t off0 = off;
-    const size_t off1 = off + 16ul;
+      const size_t off0 = off;
+      const size_t off1 = off + 16ul;
 
-    romulus_common::rho(state, data + off0, enc);
-    romulus_common::update_lfsr(lfsr);
+      romulus_common::rho(state, data + off0, enc);
+      romulus_common::update_lfsr(lfsr);
 
-    const size_t to_read = std::min(16ul, dlen - off1);
-    std::memcpy(right_blk, data + off1, to_read);
+      const size_t to_read = std::min(16ul, dlen - off1);
+      std::memcpy(right_blk, data + off1, to_read);
 
-    off = off1 + to_read;
+      off = off1 + to_read;
 
-    const size_t br1[2] = {right_blk[15], to_read};
-    right_blk[15] = br1[to_read < 16];
+      const size_t br1[2] = {right_blk[15], to_read};
+      right_blk[15] = br1[to_read < 16];
 
-    romulus_common::encode(key, right_blk, lfsr, 8, tweakey);
+      romulus_common::encode(key, right_blk, lfsr, 8, tweakey);
+
+      skinny::initialize(&st, state, tweakey);
+      skinny::tbc(&st);
+
+      std::memcpy(state, st.is, 16);
+
+      romulus_common::update_lfsr(lfsr);
+    }
+
+    const size_t to_read = dlen - off;
+
+    std::memset(last_blk, 0, 16);
+    std::memcpy(last_blk, data + off, to_read);
+
+    const size_t br2[2] = {last_blk[15], to_read};
+    last_blk[15] = br2[to_read < 16];
+
+    romulus_common::rho(state, last_blk, enc);
+
+    if (tot_blk_cnt > (half_blk_cnt << 1)) {
+      romulus_common::update_lfsr(lfsr);
+    }
+
+    constexpr size_t br3[2] = {24, 26};
+    romulus_common::encode(key, nonce, lfsr, br3[flg], tweakey);
 
     skinny::initialize(&st, state, tweakey);
     skinny::tbc(&st);
 
     std::memcpy(state, st.is, 16);
-
-    romulus_common::update_lfsr(lfsr);
   }
 
-  const size_t to_read = dlen - off;
+  romulus_common::set_lfsr(lfsr);
 
-  std::memset(last_blk, 0, 16);
-  std::memcpy(last_blk, data + off, to_read);
+  {
+    constexpr size_t br0[2] = {0, 1};
 
-  const size_t br2[2] = {last_blk[15], to_read};
-  last_blk[15] = br2[to_read < 16];
+    const size_t full_blk_cnt = ctlen >> 4;
+    const size_t rm_bytes = ctlen & 15;
 
-  romulus_common::rho(state, last_blk, enc);
+    const bool flg = (ctlen == 0) | (rm_bytes > 0);
 
-  if (tot_blk_cnt > (half_blk_cnt << 1)) {
+    const size_t tot_blk_cnt = full_blk_cnt + br0[flg];
+
+    size_t off = 0;
+
+    for (size_t i = 0; i < tot_blk_cnt - 1; i++) {
+      romulus_common::rho(state, txt + off, cipher + off);
+      romulus_common::update_lfsr(lfsr);
+
+      romulus_common::encode(key, nonce, lfsr, 4, tweakey);
+
+      skinny::initialize(&st, state, tweakey);
+      skinny::tbc(&st);
+
+      std::memcpy(state, st.is, 16);
+      off += 16;
+    }
+
+    const size_t to_read = ctlen - off;
+
+    std::memset(last_blk, 0, 16);
+    std::memcpy(last_blk, txt + off, to_read);
+
+    const size_t br1[2] = {last_blk[15], to_read};
+    last_blk[15] = br1[to_read < 16];
+
+    romulus_common::rho(state, last_blk, enc);
+
+    std::memcpy(cipher + off, enc, to_read);
+
     romulus_common::update_lfsr(lfsr);
+
+    constexpr size_t br2[2] = {20, 21};
+    romulus_common::encode(key, nonce, lfsr, br2[flg], tweakey);
+
+    skinny::initialize(&st, state, tweakey);
+    skinny::tbc(&st);
+
+    std::memcpy(state, st.is, 16);
   }
 
-  constexpr size_t br3[2] = {24, 26};
-  romulus_common::encode(key, nonce, lfsr, br3[flg], tweakey);
+  uint8_t tmp[16];
+  std::memset(tmp, 0, 16);
 
-  skinny::initialize(&st, state, tweakey);
-  skinny::tbc(&st);
-
-  std::memcpy(state, st.is, 16);
+  romulus_common::rho(state, tmp, tag);
 }
 
 }  // namespace romulus
