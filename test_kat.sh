bd3e9d61e83b9017851d730ad5ae12bca89b82c9
Anjan Roy <hello@itzmeanjan.in>
2022-08-19 16:34:24 +0400

    drive testing of Romulus-M AEAD using KAT
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test_kat.sh
+++ test_kat.sh
@@ -12,22 +12,34 @@ pushd tmp
 wget -O romulus.zip https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-submissions/romulus.zip
 unzip romulus.zip
 
-cp romulus/Implementations/crypto_aead/romulush/LWC_HASH_KAT_256.txt ../
-cp romulus/Implementations/crypto_aead/romulusn/LWC_AEAD_KAT_128_128.txt ../
+cp romulus/Implementations/crypto_aead/romulush/LWC_HASH_KAT_256.txt ../romulush.txt
+cp romulus/Implementations/crypto_aead/romulusn/LWC_AEAD_KAT_128_128.txt ../romulusn.txt
+cp romulus/Implementations/crypto_aead/romulusm/LWC_AEAD_KAT_128_128.txt ../romulusm.txt
+cp romulus/Implementations/crypto_aead/romulust/LWC_AEAD_KAT_128_128.txt ../romulust.txt
 
 popd
 
 # ---
 
 rm -rf tmp
-mv LWC_HASH_KAT_256.txt wrapper/python/
-mv LWC_AEAD_KAT_128_128.txt wrapper/python/
+mv romulus{h,n,m,t}.txt wrapper/python/
 
 # ---
 
 pushd wrapper/python
 
-python3 -m pytest -v
+mv romulush.txt LWC_HASH_KAT_256.txt
+python3 -m pytest -k romulush -v
+
+mv romulusn.txt LWC_AEAD_KAT_128_128.txt
+python3 -m pytest -k romulusn -v
+
+mv romulusm.txt LWC_AEAD_KAT_128_128.txt
+python3 -m pytest -k romulusm -v
+
+mv romulust.txt LWC_AEAD_KAT_128_128.txt
+python3 -m pytest -k romulust -v
+
 rm LWC_*_KAT_*.txt
 
 popd
