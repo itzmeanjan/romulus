49c7f743325812170701b19df9889963e606cb01
Anjan Roy <hello@itzmeanjan.in>
2022-08-19 16:33:58 +0400

    test functional correctness of Romulus-M AEAD using Known Answer Tests
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- wrapper/python/test_romulus.py
+++ wrapper/python/test_romulus.py
@@ -81,7 +81,7 @@ def test_romulusn_kat():
 
             # 128 -bit secret key
             key = int(f"0x{key}", base=16).to_bytes(16, "big")
-            # 256 -bit public message nonce
+            # 128 -bit public message nonce
             nonce = int(f"0x{nonce}", base=16).to_bytes(16, "big")
             # plain text
             pt = bytes(
@@ -110,10 +110,79 @@ def test_romulusn_kat():
 
             assert (
                 cipher + tag == ct
-            ), f"[Romulus-N KAT {cnt}] expected cipher to be 0x{ct.hex()}, found 0x${(cipher + tag).hex()} !"
+            ), f"[Romulus-N KAT {cnt}] expected cipher to be 0x{ct.hex()}, found 0x{(cipher + tag).hex()} !"
             assert (
                 pt == text and flag
             ), f"[Romulus-N KAT {cnt}] expected plain text 0x{pt.hex()}, found 0x{text.hex()} !"
+
+            # don't need this line, so discard
+            fd.readline()
+
+
+def test_romulusm_kat():
+    """
+    Tests functional correctness of Romulus-M AEAD implementation, using
+    Known Answer Tests submitted along with final round submission of Romulus
+    in NIST LWC
+
+    See https://csrc.nist.gov/projects/lightweight-cryptography/finalists
+    """
+    with open("LWC_AEAD_KAT_128_128.txt", "r") as fd:
+        while True:
+            cnt = fd.readline()
+            if not cnt:
+                # no more KATs remaining
+                break
+
+            key = fd.readline()
+            nonce = fd.readline()
+            pt = fd.readline()
+            ad = fd.readline()
+            ct = fd.readline()
+
+            # extract out required fields
+            cnt = int([i.strip() for i in cnt.split("=")][-1])
+            key = [i.strip() for i in key.split("=")][-1]
+            nonce = [i.strip() for i in nonce.split("=")][-1]
+            pt = [i.strip() for i in pt.split("=")][-1]
+            ad = [i.strip() for i in ad.split("=")][-1]
+            ct = [i.strip() for i in ct.split("=")][-1]
+
+            # 128 -bit secret key
+            key = int(f"0x{key}", base=16).to_bytes(16, "big")
+            # 128 -bit public message nonce
+            nonce = int(f"0x{nonce}", base=16).to_bytes(16, "big")
+            # plain text
+            pt = bytes(
+                [
+                    int(f"0x{pt[(i << 1): ((i+1) << 1)]}", base=16)
+                    for i in range(len(pt) >> 1)
+                ]
+            )
+            # associated data
+            ad = bytes(
+                [
+                    int(f"0x{ad[(i << 1): ((i+1) << 1)]}", base=16)
+                    for i in range(len(ad) >> 1)
+                ]
+            )
+            # cipher text + authentication tag ( expected )
+            ct = bytes(
+                [
+                    int(f"0x{ct[(i << 1): ((i+1) << 1)]}", base=16)
+                    for i in range(len(ct) >> 1)
+                ]
+            )
+
+            cipher, tag = romulus.romulusm_encrypt(key, nonce, ad, pt)
+            flag, text = romulus.romulusm_decrypt(key, nonce, tag, ad, cipher)
+
+            assert (
+                cipher + tag == ct
+            ), f"[Romulus-M KAT {cnt}] expected cipher to be 0x{ct.hex()}, found 0x{(cipher + tag).hex()} !"
+            assert (
+                pt == text and flag
+            ), f"[Romulus-M KAT {cnt}] expected plain text 0x{pt.hex()}, found 0x{text.hex()} !"
 
             # don't need this line, so discard
             fd.readline()
