1738144cc50ede185e6088529b22685c623fa394
Anjan Roy <hello@itzmeanjan.in>
2022-08-27 11:48:06 +0400

    updated Romulus-N to reduce memcpy, when initializing tweakable block cipher state
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/romulusn.hpp
+++ include/romulusn.hpp
@@ -24,17 +24,13 @@ inline static void encrypt(
     const size_t ctlen,                     // len(txt) = len(cipher) | >= 0
     uint8_t* const __restrict tag           // 128 -bit authentication tag
 ) {
-  uint8_t state[16];
+  skinny::state_t st;
+
   uint8_t lfsr[7];
-
   uint8_t enc[16];
-  uint8_t tweakey[48];
-
   uint8_t last_blk[16];
 
-  skinny::state st;
-
-  std::memset(state, 0, 16);
+  std::memset(st.arr, 0, 16);
   romulus_common::set_lfsr(lfsr);
 
   {
@@ -57,7 +53,7 @@ inline static void encrypt(
       const size_t off0 = off;
       const size_t off1 = off + 16ul;
 
-      romulus_common::rho(state, data + off0, enc);
+      romulus_common::rho(st.arr, data + off0, enc);
       romulus_common::update_lfsr(lfsr);
 
       const size_t to_read = std::min(16ul, dlen - off1);
@@ -68,13 +64,9 @@ inline static void encrypt(
       const size_t br1[2] = {right_blk[15], to_read};
       right_blk[15] = br1[to_read < 16];
 
-      romulus_common::encode(key, right_blk, lfsr, 8, tweakey);
+      romulus_common::encode(key, right_blk, lfsr, 8, st.arr + 16);
 
-      skinny::initialize(&st, state, tweakey);
       skinny::tbc(&st);
-
-      std::memcpy(state, st.is, 16);
-
       romulus_common::update_lfsr(lfsr);
     }
 
@@ -86,19 +78,16 @@ inline static void encrypt(
     const size_t br2[2] = {last_blk[15], to_read};
     last_blk[15] = br2[to_read < 16];
 
-    romulus_common::rho(state, last_blk, enc);
+    romulus_common::rho(st.arr, last_blk, enc);
 
     if (tot_blk_cnt > (half_blk_cnt << 1)) {
       romulus_common::update_lfsr(lfsr);
     }
 
     constexpr size_t br3[2] = {24, 26};
-    romulus_common::encode(key, nonce, lfsr, br3[flg], tweakey);
+    romulus_common::encode(key, nonce, lfsr, br3[flg], st.arr + 16);
 
-    skinny::initialize(&st, state, tweakey);
     skinny::tbc(&st);
-
-    std::memcpy(state, st.is, 16);
   }
 
   romulus_common::set_lfsr(lfsr);
@@ -116,15 +105,12 @@ inline static void encrypt(
     size_t off = 0;
 
     for (size_t i = 0; i < tot_blk_cnt - 1; i++) {
-      romulus_common::rho(state, txt + off, cipher + off);
+      romulus_common::rho(st.arr, txt + off, cipher + off);
       romulus_common::update_lfsr(lfsr);
 
-      romulus_common::encode(key, nonce, lfsr, 4, tweakey);
+      romulus_common::encode(key, nonce, lfsr, 4, st.arr + 16);
 
-      skinny::initialize(&st, state, tweakey);
       skinny::tbc(&st);
-
-      std::memcpy(state, st.is, 16);
       off += 16;
     }
 
@@ -136,25 +122,22 @@ inline static void encrypt(
     const size_t br1[2] = {last_blk[15], to_read};
     last_blk[15] = br1[to_read < 16];
 
-    romulus_common::rho(state, last_blk, enc);
+    romulus_common::rho(st.arr, last_blk, enc);
 
     std::memcpy(cipher + off, enc, to_read);
 
     romulus_common::update_lfsr(lfsr);
 
     constexpr size_t br2[2] = {20, 21};
-    romulus_common::encode(key, nonce, lfsr, br2[flg], tweakey);
+    romulus_common::encode(key, nonce, lfsr, br2[flg], st.arr + 16);
 
-    skinny::initialize(&st, state, tweakey);
     skinny::tbc(&st);
-
-    std::memcpy(state, st.is, 16);
   }
 
   uint8_t tmp[16];
   std::memset(tmp, 0, 16);
 
-  romulus_common::rho(state, tmp, tag);
+  romulus_common::rho(st.arr, tmp, tag);
 }
 
 // Given 16 -bytes secret key, 16 -bytes nonce, 16 -bytes authentication tag, N
@@ -174,17 +157,13 @@ inline static bool decrypt(
     uint8_t* const __restrict txt,           // N -bytes plain text
     const size_t ctlen                       // len(cipher) = len(txt) | >= 0
 ) {
-  uint8_t state[16];
+  skinny::state_t st;
+
   uint8_t lfsr[7];
-
   uint8_t enc[16];
-  uint8_t tweakey[48];
-
   uint8_t last_blk[16];
 
-  skinny::state st;
-
-  std::memset(state, 0, 16);
+  std::memset(st.arr, 0, 16);
   romulus_common::set_lfsr(lfsr);
 
   {
@@ -207,7 +186,7 @@ inline static bool decrypt(
       const size_t off0 = off;
       const size_t off1 = off + 16ul;
 
-      romulus_common::rho(state, data + off0, enc);
+      romulus_common::rho(st.arr, data + off0, enc);
       romulus_common::update_lfsr(lfsr);
 
       const size_t to_read = std::min(16ul, dlen - off1);
@@ -218,13 +197,9 @@ inline static bool decrypt(
       const size_t br1[2] = {right_blk[15], to_read};
       right_blk[15] = br1[to_read < 16];
 
-      romulus_common::encode(key, right_blk, lfsr, 8, tweakey);
+      romulus_common::encode(key, right_blk, lfsr, 8, st.arr + 16);
 
-      skinny::initialize(&st, state, tweakey);
       skinny::tbc(&st);
-
-      std::memcpy(state, st.is, 16);
-
       romulus_common::update_lfsr(lfsr);
     }
 
@@ -236,19 +211,16 @@ inline static bool decrypt(
     const size_t br2[2] = {last_blk[15], to_read};
     last_blk[15] = br2[to_read < 16];
 
-    romulus_common::rho(state, last_blk, enc);
+    romulus_common::rho(st.arr, last_blk, enc);
 
     if (tot_blk_cnt > (half_blk_cnt << 1)) {
       romulus_common::update_lfsr(lfsr);
     }
 
     constexpr size_t br3[2] = {24, 26};
-    romulus_common::encode(key, nonce, lfsr, br3[flg], tweakey);
+    romulus_common::encode(key, nonce, lfsr, br3[flg], st.arr + 16);
 
-    skinny::initialize(&st, state, tweakey);
     skinny::tbc(&st);
-
-    std::memcpy(state, st.is, 16);
   }
 
   romulus_common::set_lfsr(lfsr);
@@ -266,15 +238,12 @@ inline static bool decrypt(
     size_t off = 0;
 
     for (size_t i = 0; i < tot_blk_cnt - 1; i++) {
-      romulus_common::rho_inv(state, cipher + off, txt + off);
+      romulus_common::rho_inv(st.arr, cipher + off, txt + off);
       romulus_common::update_lfsr(lfsr);
 
-      romulus_common::encode(key, nonce, lfsr, 4, tweakey);
+      romulus_common::encode(key, nonce, lfsr, 4, st.arr + 16);
 
-      skinny::initialize(&st, state, tweakey);
       skinny::tbc(&st);
-
-      std::memcpy(state, st.is, 16);
       off += 16;
     }
 
@@ -286,10 +255,10 @@ inline static bool decrypt(
     uint8_t gs[16];
 
     for (size_t i = 0; i < 16; i++) {
-      const uint8_t b7 = state[i] >> 7;
-      const uint8_t b0 = state[i] & 1;
+      const uint8_t b7 = st.arr[i] >> 7;
+      const uint8_t b0 = st.arr[i] & 1;
 
-      gs[i] = ((b7 ^ b0) << 7) | (state[i] >> 1);
+      gs[i] = ((b7 ^ b0) << 7) | (st.arr[i] >> 1);
     }
 
     std::memcpy(state_prime + to_read, gs + to_read, 16 - to_read);
@@ -304,19 +273,16 @@ inline static bool decrypt(
       last_blk[i] ^= state_prime[i];
     }
 
-    romulus_common::rho_inv(state, last_blk, enc);
+    romulus_common::rho_inv(st.arr, last_blk, enc);
 
     std::memcpy(txt + off, enc, to_read);
 
     romulus_common::update_lfsr(lfsr);
 
     constexpr size_t br2[2] = {20, 21};
-    romulus_common::encode(key, nonce, lfsr, br2[flg], tweakey);
+    romulus_common::encode(key, nonce, lfsr, br2[flg], st.arr + 16);
 
-    skinny::initialize(&st, state, tweakey);
     skinny::tbc(&st);
-
-    std::memcpy(state, st.is, 16);
   }
 
   uint8_t tmp[16];
@@ -325,7 +291,7 @@ inline static bool decrypt(
   std::memset(tmp, 0, 16);
   std::memset(tag_, 0, 16);
 
-  romulus_common::rho(state, tmp, tag_);
+  romulus_common::rho(st.arr, tmp, tag_);
 
   bool flg = false;
   for (size_t i = 0; i < 16; i++) {
